436af5ae85721b63fb0a2fe801bcf6f3bf36ad21
letha <secret@mail.net>
2021-11-25 15:05:50 +0000

    Add seeders for User, Comment model.

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,14 +17,17 @@ class UserSeeder extends Seeder
         $inserts = [
             [
                 'name' => 'Irene',
+                'email' => Str::random(10) . '@secret.net',
                 'password' => Hash::make(Str::random(10)),
             ],
             [
                 'name' => 'Danielle',
+                'email' => Str::random(10) . '@secret.net',
                 'password' => Hash::make(Str::random(10)),
             ],
             [
                 'name' => 'Daniel',
+                'email' => Str::random(10) . '@secret.net',
                 'password' => Hash::make(Str::random(10)),
             ],
         ];
