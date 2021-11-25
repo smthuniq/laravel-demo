cca6da9d93dcb644cce84b121985d00c11b21911
letha <secret@mail.net>
2021-11-25 14:42:32 +0000

    Add controllers for Comment model api requests ('index' method).

---
+++ app/Http/Controllers/UserCommentController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Comment;
+use Illuminate\Http\Request;
+
+class UserCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, $userId)
+    {
+        $userIdType = $request->query('user_id_type');
+        $allowedIdTypes = [
+            'name',
+        ];
+
+        $commentsQuery = Comment::with('user:id,name')->take(10);
+        // it allows e.g. to search comments by user name or another allowed column
+        if (in_array($userIdType, $allowedIdTypes, true)) {
+            $commentsQuery = $commentsQuery
+            ->whereHas('user', function ($query) use ($userId, $userIdType) {
+                $query->where($userIdType, 'LIKE', "$userId%");
+            });
+        }
+        $comments = $commentsQuery->get(['id', 'user_id', 'content']);
+        
+        if (!count($comments)) {
+            return response()->json($comments, 404);
+        }
+        return response()->json($comments->toArray());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
